d0c903bb3561979fb640dca225a2645854d268c1
Erik Skultety <eskultet@redhat.com>
2025-04-01 15:04:22 +0200

    tests: integration: Introduce legacy entrypoint tests
    With the rebranding part done, one part that is still missing is an
    integration test for the legacy entrypoint - cachi2.
    
    There isn't much of a wiggle room currently in our integration test
    suite to test this properly since the code base has now been completely
    migrated to the new name - hermeto - with the only differing bit are
    public facing strings and default locations.
    
    Therefore, 2 tests are introduced:
    1) a trivial test checking our CLI help messages are consistent with
       the entry point used
    2) an e2e test that replicates an existing Yarn test with a couple of
       differences - changed filesystem locations and an overriden
       container image entry point
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

---
+++ tests/integration/test_legacy.py
@@ -0,0 +1,90 @@
+import logging
+from pathlib import Path
+
+import pytest
+
+from . import utils
+
+log = logging.getLogger(__name__)
+
+
+def test_help(hermeto_image: utils.ContainerImage, tmp_path: Path) -> None:
+    """
+    Simple test to verify if there's only the expected naming in our help.
+
+    TODO: Drop this when we no longer support the legacy entrypoint.
+    """
+    for cmd in ("fetch-deps", "generate-env", "inject-files", "merge-sboms"):
+        output, exit_code = hermeto_image.run_cmd_on_image(
+            [cmd, "--help"], tmp_path, podman_flags=["--entrypoint=cachi2"]
+        )
+
+        assert exit_code == 0, f"Querying help failed, output-cmd: {output}"
+        for line in output.split(sep="\n"):
+            line = line.strip()
+            if cmd in line:
+                assert "cachi2" in line and not ("hermeto" in line or "APP_NAME" in line)
+        if cmd == "fetch-deps":
+            assert "[default: ./cachi2-output]" in output
+
+
+@pytest.mark.parametrize(
+    "test_params,check_cmd,expected_cmd_output",
+    [
+        pytest.param(
+            utils.TestParameters(
+                branch="cargo/mixed-git-crate-dependency",
+                packages=({"path": ".", "type": "cargo"},),
+                check_output=True,
+                check_deps_checksums=False,
+                check_vendor_checksums=False,
+                expected_exit_code=0,
+                expected_output="",
+            ),
+            [],  # No additional commands are run to verify the build
+            [],
+            id="legacy_entrypoint_e2e_test",
+        ),
+    ],
+)
+def test_e2e_cargo(
+    test_params: utils.TestParameters,
+    check_cmd: list[str],
+    expected_cmd_output: str,
+    hermeto_image: utils.ContainerImage,
+    tmp_path: Path,
+    test_repo_dir: Path,
+    test_data_dir: Path,
+    request: pytest.FixtureRequest,
+) -> None:
+    """
+    End to end test for cargo using the legacy entrypoint.
+
+    TODO: Drop this when we no longer support the legacy entrypoint.
+    """
+    test_case = request.node.callspec.id
+
+    podman_entrypoint = "--entrypoint=cachi2"
+    utils.fetch_deps_and_check_output(
+        tmp_path,
+        test_case,
+        test_params,
+        test_repo_dir,
+        test_data_dir,
+        hermeto_image,
+        fetch_output_dirname="cachi2-output",
+        podman_flags=[podman_entrypoint],
+    )
+
+    utils.build_image_and_check_cmd(
+        tmp_path,
+        test_repo_dir,
+        test_data_dir,
+        test_case,
+        check_cmd,
+        expected_cmd_output,
+        hermeto_image,
+        fetch_output_dirname="cachi2-output",
+        env_vars_filename="cachi2.env",
+        podman_flags=[podman_entrypoint],
+    )

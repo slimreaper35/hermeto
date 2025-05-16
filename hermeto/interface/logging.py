378d7368b5d8dd6bb592e48c900d5e990be6e34c
Erik Skultety <eskultet@redhat.com>
2025-05-16 14:56:10 +0200

    logging: Introduce a new logger adapter
    In order to selectively report a warning rather than an error in cases
    where we wish to apply the effect of the --mode=permissive CLI flag we
    can either introduce a bunch of if-else clauses in all places where
    applicable or we can instead opt for an idiomatic solution involving
    logger adapters [1] which allow contextual changes to the data being
    reported and how they're logged. The advantage of a logger adapter is
    that it's essentially a wrapper over a regular logger instance and so
    it implements the same public interface. That's perfect for our cause
    because we can define a new method that would contextually report a
    warning or an error in scenarios where the CLI flag should take effect.
    All other places would continue functioning identically via existing
    logger invocations thanks to the public interface compatibility.
    
    [1] https://docs.python.org/3/howto/logging-cookbook.html#using-loggeradapters-to-impart-contextual-information
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- hermeto/interface/logging.py
+++ hermeto/interface/logging.py
@@ -1,6 +1,8 @@
 import enum
 import logging
-from typing import Iterable
+from typing import Any, Iterable
+
+from hermeto.core.models.input import Mode
 
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 
@@ -13,6 +15,31 @@ class LogLevel(str, enum.Enum):
     WARNING = "WARNING"
     ERROR = "ERROR"
     CRITICAL = "CRITICAL"
+
+
+class EnforcingModeLoggerAdapter(logging.LoggerAdapter):
+    """
+    Enforcing mode aware logger adapter.
+
+    This adapter is to be used as the logger wrapper providing functionality to
+    decide whether to log a warning or an error based on context and on the CLI mode setting.
+    """
+
+    def error_or_warn(self, msg: str, *args: Any, enforcing_mode: Mode, **kwargs: Any) -> None:
+        """
+        Log an error or a warning based on the CLI enforcing mode setting.
+
+        We don't want all errors converted to warnings, most of them will always
+        be fatal, so this warning/error wrapper is just an addition to the standard set of logger
+        methods for cases where context is the decisive factor.
+        """
+        # NOTE: We should probably drop the enforcing_mode argument in favour of e.g. a Singleton
+        # settings instance.
+        msg = f"[mode:{str(enforcing_mode).upper()}] {msg}"
+        if enforcing_mode == Mode.PERMISSIVE:
+            self.warning(msg, *args, **kwargs)
+        else:
+            self.error(msg, *args, **kwargs)
 
 
 def setup_logging(level: LogLevel, additional_modules: Iterable[str] = ()) -> None:

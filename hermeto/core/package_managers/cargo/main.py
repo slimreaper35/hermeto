1281c00f5dbb774fe65631aa52625e3215e91b51
Michal Šoltis <msoltis@redhat.com>
2025-06-20 14:43:51 +0200

    cargo: Refactor toml files parsing logic
    Improve parsing of Cargo.toml, Cargo.lock and .cargo/config.toml files
    by separating their logic into a distinct function. This enhances
    code clarity without unnecessary comments and removes duplication.
    
    Change type of `version` field from `str` to `Optional[str]` to silence
    mypy error. The class is used for "main package" component, which can be
    a virtual workspace without defined package name and version in
    Cargo.toml. The version is None in that case. In the future patches,
    I will revert this line back to `str` type and move the logic for "main
    package" component away from this class.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- hermeto/core/package_managers/cargo/main.py
+++ hermeto/core/package_managers/cargo/main.py
@@ -7,11 +7,10 @@ from dataclasses import dataclass
 from functools import cached_property
 from itertools import chain
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 import tomlkit
 from packageurl import PackageURL
-from tomlkit.toml_file import TOMLFile
 
 from hermeto.core.errors import NotAGitRepo, PackageRejected
 from hermeto.core.models.input import Mode, Request
@@ -45,7 +44,7 @@ class CargoPackage:
     """CargoPackage."""
 
     name: str
-    version: str
+    version: Optional[str] = None
     source: Optional[str] = None  # [git|registry]+https://github.com/<org>/<package>#[|<sha>]
     checksum: Optional[str] = None
     dependencies: Optional[list] = None
@@ -87,35 +86,29 @@ def fetch_cargo_source(request: Request) -> RequestOutput:
     return RequestOutput.from_obj_list(components, environment_variables, project_files)
 
 
-def _extract_package_info(path_to_toml: Path) -> dict:
-    # 'value' unwraps the underlying dict and that makes mypy happy (it complains about
-    # mismatching type otherwise despite parsed document having the necessary interface).
-    parsed_toml = tomlkit.parse(path_to_toml.read_text())
-    package_info = parsed_toml.value["package"]
-    if isinstance(package_info, dict) and package_info.get("version") == {"workspace": True}:
+def _parse_toml_project_file(path: Path) -> dict[str, Any]:
+    """Parse any Cargo related TOML file into a dictionary."""
+    parsed_toml = tomlkit.parse(path.read_text())
+    return parsed_toml.value
+
+
+def _resolve_main_package(package_dir: RootedPath) -> tuple[str, Optional[str]]:
+    """Resolve package name and version from Cargo.toml."""
+    parsed_toml = _parse_toml_project_file(package_dir.path / "Cargo.toml")
+    package_info = parsed_toml.get("package", {})
+
+    # use default values if the project is a virtual workspace without any package information
+    name = package_info.get("name", package_dir.path.stem)
+    version = package_info.get("version", None)
+
+    if package_info.get("version") == {"workspace": True}:
         # support for workspace package table https://doc.rust-lang.org/cargo/reference/workspaces.html#the-package-table
         # real world example: https://github.com/pyca/cryptography/blob/43.0.0/src/rust/Cargo.toml
         # we condition this to when package_info is a dict because this same function has usages where package_info is a list
         # (Cargo.lock) and this info is only available when it is a dict (Cargo.toml)
-        package_info["version"] = parsed_toml.value["workspace"]["package"]["version"]
-    return package_info
+        version = parsed_toml["workspace"]["package"]["version"]
 
-
-def _resolve_main_package(package_dir: RootedPath) -> dict:
-    try:
-        return _extract_package_info(package_dir.path / "Cargo.toml")
-    # We'll get here in the case of virtual workspaces. A real-world example is
-    # https://github.com/rwf2/Rocket/tree/master
-    # In this case there is no package name per se, but there still is a metapackage.
-    # We'll use directory name as a fallback in this case.
-    # Version won't make much sense here: this is a meta-package, the state of a
-    # repository will be captured in VCS_URL, and individual components will be
-    # versioned.
-    except KeyError:
-        return {
-            "name": package_dir.path.stem,
-            "version": None,
-        }
+    return name, version
 
 
 def _verify_lockfile_is_present_or_fail(package_dir: RootedPath) -> None:
@@ -222,9 +215,11 @@ def _resolve_cargo_package(
             cmd=cmd, params={"cwd": package_dir}, package_dir=package_dir, mode=request.mode
         )
 
-    packages = _extract_package_info(package_dir.path / "Cargo.lock")
-    main_package = _resolve_main_package(package_dir)
-    is_a_dep = lambda p: p["name"] != main_package["name"]
+    parsed_lockfile = _parse_toml_project_file(package_dir.path / "Cargo.lock")
+    packages = parsed_lockfile.get("package", [])
+
+    main_package_name, main_package_version = _resolve_main_package(package_dir)
+    is_a_dep = lambda p: p["name"] != main_package_name
     try:
         vcs_url = get_repo_id(package_dir.root).as_vcs_url_qualifier()
     except NotAGitRepo:
@@ -234,7 +229,7 @@ def _resolve_cargo_package(
         CargoPackage(**p, vcs_url=vcs_url).to_component() for p in packages if is_a_dep(p)
     )
     main_component = CargoPackage(
-        name=main_package["name"], version=main_package["version"], vcs_url=vcs_url
+        name=main_package_name, version=main_package_version, vcs_url=vcs_url
     ).to_component()
 
     components = chain((main_component,), deps_components)
@@ -269,10 +264,8 @@ def _use_vendored_sources(package_dir: RootedPath, config_template: dict) -> Pro
     # Python extension it is better to check if there is an old-style config present and
     # process it if found.
     cfn = ".cargo/config" if _old_style_config_is_present_in(package_dir) else ".cargo/config.toml"
-    cargo_config = package_dir.join_within_root(cfn)
+    config_path = package_dir.join_within_root(cfn).path
 
-    toml_file = TOMLFile(cargo_config)
-    original_content = toml_file.read() if cargo_config.path.exists() else {}
+    original_content = _parse_toml_project_file(config_path) if config_path.exists() else {}
     original_content.update(config_template)
-
-    return ProjectFile(abspath=cargo_config.path, template=tomlkit.dumps(config_template))
+    return ProjectFile(abspath=config_path, template=tomlkit.dumps(config_template))

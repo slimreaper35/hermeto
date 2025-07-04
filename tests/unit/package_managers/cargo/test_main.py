7d89d2a5c31b4a129bc0dc67dfdb106f75fa3c69
Michal Šoltis <msoltis@redhat.com>
2025-07-04 13:45:13 +0200

    cargo: Simplify purls
    Improve the handling of cargo packages by separating local dependencies
    from registry/git ones. This change introduces a new LocalCargoPackage
    class to better represent local dependencies and workspace packages.
    The SBOM generation logic is updated to properly distinguish between
    different package types and include relevant metadata like VCS URL and
    subpath.
    
    The `vcs_url` qualifier was incorrectly set only for main packages
    while dependencies from git sources were missing this qualifier.
    This caused inconsistent purl generation.
    
    We now derive `vcs_url` directly from git source URLs for dependencies
    by parsing and reconstructing the repository URL with its ref. The
    main package still uses the project's VCS URL when available.
    
    VCS URL format is defined in PURL specification:
    https://github.com/package-url/purl-spec/blob/main/PURL-SPECIFICATION.rst
    
    Remove non-existent source field from PURL qualifiers.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/package_managers/cargo/test_main.py
+++ tests/unit/package_managers/cargo/test_main.py
@@ -1,4 +1,8 @@
-from hermeto.core.package_managers.cargo.main import _resolve_main_package
+from typing import Any
+
+import pytest
+
+from hermeto.core.package_managers.cargo.main import CargoPackage, _resolve_main_package
 from hermeto.core.rooted_path import RootedPath
 
 
@@ -52,3 +56,40 @@ def test_virtual_workspace_without_workspace_version(rooted_tmp_path: RootedPath
     name, version = _resolve_main_package(rooted_tmp_path)
     assert name == expected_name
     assert version is None
+
+
+@pytest.mark.parametrize(
+    "pkg, expected_purl",
+    [
+        pytest.param(
+            {
+                "name": "foo",
+                "version": "0.1.0",
+            },
+            "pkg:cargo/foo@0.1.0",
+            id="simple_package",
+        ),
+        pytest.param(
+            {
+                "name": "foo",
+                "version": "0.1.0",
+                "source": "registry+https://github.com/rust-lang/crates.io-index",
+                "checksum": "abc123",
+            },
+            "pkg:cargo/foo@0.1.0?checksum=abc123",
+            id="package_with_registry_source_and_checksum",
+        ),
+        pytest.param(
+            {
+                "name": "foo",
+                "version": "0.1.0",
+                "source": "git+https://github.com/rust-random/rand?rev=abc123#abc123",
+            },
+            "pkg:cargo/foo@0.1.0?vcs_url=git%2Bhttps://github.com/rust-random/rand%40abc123",
+            id="package_with_git_source",
+        ),
+    ],
+)
+def test_cargo_package_purl_generation(pkg: dict[str, Any], expected_purl: str) -> None:
+    package = CargoPackage(**pkg)
+    assert package.purl.to_string() == expected_purl

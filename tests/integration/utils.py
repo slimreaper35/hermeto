94def96cb19934745250b4a6c7fc5ea3447cc2ad
Bruno Pimentel <bpimente@redhat.com>
2025-07-31 10:57:17 +0200

    integration tests: Remove unused `run_cmd` function
    This was replaced by `ContainerEngine.run_cmd` and is no longer needed.
    
    Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -5,7 +5,6 @@ import json
 import logging
 import os
 import shutil
-import subprocess
 import sys
 import tempfile
 from collections.abc import Sequence
@@ -190,31 +189,6 @@ def _build_image(podman_cmd: list[str], *, tag: str) -> ContainerImage:
     if exit_code != 0:
         raise RuntimeError(f"Building image failed. Output:\n{output}")
     return ContainerImage(tag)
-
-
-def run_cmd(cmd: Union[list[str], str], **subprocess_kwargs: Any) -> tuple[str, int]:
-    """
-    Run command via subprocess.
-
-    :param cmd: command to be executed
-    :param subprocess_kwargs: passthrough kwargs to subprocess.run
-    :return: Command output and exitcode
-    :rtype: Tuple
-    """
-    log.info("Run command: %s.", cmd)
-
-    # redirect stderr to stdout for easier evaluation/handling of a single stream
-    forced_options = {
-        "stdout": subprocess.PIPE,
-        "stderr": subprocess.STDOUT,
-        "encoding": "utf-8",
-        "text": True,
-    }
-
-    subprocess_kwargs.update(forced_options)
-    process = subprocess.run(cmd, **subprocess_kwargs)
-
-    return process.stdout, process.returncode
 
 
 def _calculate_files_checksums_in_dir(root_dir: Path) -> dict:

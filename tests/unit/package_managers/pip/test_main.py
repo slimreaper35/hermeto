4188f43903c8dc15a0e3d1ca1ff07f9771ed8674
Michal Šoltis <msoltis@redhat.com>
2025-10-02 15:21:03 +0200

    pip: Remove unreachable scenario from test_fetch_pip_source
    One of the pytest parameters is a test to prefetch gomod artifacts for
    a pip project. This cannot happen. Each "fetch_x_source" is connected
    with the respective package manager name that is used in the CLI.
    
    I haven't found a similar test or piece of code elsewhere for other
    package managers.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/package_managers/pip/test_main.py
+++ tests/unit/package_managers/pip/test_main.py
@@ -1303,11 +1303,6 @@ def test_replace_external_requirements(
     "packages, n_pip_packages",
     [
         pytest.param(
-            [{"type": "gomod"}],
-            0,
-            id="not_python_project",
-        ),
-        pytest.param(
             [{"type": "pip", "requirements_files": ["requirements.txt"]}],
             1,
             id="single_python_package",
@@ -1463,10 +1458,7 @@ def test_fetch_pip_source(
         ),
     ]
 
-    if n_pip_packages == 0:
-        expect_packages = []
-        expect_files = []
-    elif n_pip_packages == 1:
+    if n_pip_packages == 1:
         expect_packages = expect_components_package_a
         expect_files = [replaced_file_a]
     elif n_pip_packages == 2:
@@ -1477,15 +1469,15 @@ def test_fetch_pip_source(
 
     assert output.components == expect_packages
     assert output.build_config.project_files == expect_files
-    assert len(output.build_config.environment_variables) == (2 if n_pip_packages > 0 else 0)
+    assert len(output.build_config.environment_variables) == 2
 
-    if n_pip_packages >= 1:
+    if n_pip_packages == 1:
         mock_resolve_pip.assert_any_call(
             source_dir, output_dir, [Path("requirements.txt")], None, None
         )
         mock_replace_requirements.assert_any_call("/package_a/requirements.txt")
         mock_replace_requirements.assert_any_call("/package_a/requirements-build.txt")
-    if n_pip_packages >= 2:
+    if n_pip_packages == 2:
         mock_resolve_pip.assert_any_call(
             source_dir.join_within_root("foo"), output_dir, None, [], None
         )

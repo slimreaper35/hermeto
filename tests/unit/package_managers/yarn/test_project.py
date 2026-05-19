082810c1f83c66efce8dbbc47850863731a4ffd7
Michal Šoltis <msoltis@redhat.com>
2026-05-19 18:35:06 +0200

    yarn berry: Use shared PackageJson class
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- tests/unit/package_managers/yarn/test_project.py
+++ tests/unit/package_managers/yarn/test_project.py
@@ -12,8 +12,8 @@ from hermeto.core.errors import (
     PackageRejected,
     UnexpectedFormat,
 )
+from hermeto.core.package_managers.javascript.package_json import PackageJson
 from hermeto.core.package_managers.yarn.project import (
-    PackageJson,
     Project,
     YarnRc,
     get_semver_from_package_manager,
@@ -135,7 +135,7 @@ def test_write_yarnrc(rooted_tmp_path: RootedPath) -> None:
 
 
 def _prepare_package_json_file(rooted_tmp_path: RootedPath, data: str) -> PackageJson:
-    path = rooted_tmp_path.join_within_root("package.json")
+    path = rooted_tmp_path.path.joinpath("package.json")
 
     with open(path, "w") as f:
         f.write(data)
@@ -186,7 +186,7 @@ def test_parse_project_folder(rooted_tmp_path: RootedPath) -> None:
 
     assert project.yarn_rc is not None
     assert project.yarn_rc._path == rooted_tmp_path.join_within_root(".yarnrc.yml")
-    assert project.package_json._path == rooted_tmp_path.join_within_root("package.json")
+    assert project.package_json.path == rooted_tmp_path.join_within_root("package.json").path
 
 
 def test_parse_project_folder_without_yarnrc(rooted_tmp_path: RootedPath) -> None:
@@ -198,7 +198,7 @@ def test_parse_project_folder_without_yarnrc(rooted_tmp_path: RootedPath) -> Non
 
     assert len(project.yarn_rc.data) == 0
     assert project.yarn_rc._path == rooted_tmp_path.join_within_root(".yarnrc.yml")
-    assert project.package_json._path == rooted_tmp_path.join_within_root("package.json")
+    assert project.package_json.path == rooted_tmp_path.join_within_root("package.json").path
 
 
 def test_parse_empty_folder(rooted_tmp_path: RootedPath) -> None:

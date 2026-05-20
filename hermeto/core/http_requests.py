455f02841c3ef90ccf2976853aa585c4b0c560a6
Harsh Dahiya <dahiyaharsh2005@gmail.com>
2026-05-20 20:06:03 +0530

    chore(http): drop read, connect and total - default retry options
    per urllib3 docs [1], total takes precedence over read and connect
    when set, making them redundant since the user configurable `max_retries`
    always sets total.
    
    [1] https://urllib3.readthedocs.io/en/stable/reference/urllib3.util.html
    
    Signed-off-by: Harsh Dahiya <dahiyaharsh2005@gmail.com>

--- hermeto/core/http_requests.py
+++ hermeto/core/http_requests.py
@@ -12,9 +12,6 @@ ALL_REQUEST_METHODS = frozenset(
 # The set includes only methods which don't modify state of the service.
 SAFE_REQUEST_METHODS = frozenset({"GET", "HEAD", "OPTIONS", "TRACE"})
 DEFAULT_RETRY_OPTIONS: dict[str, Any] = {
-    "total": 5,
-    "read": 5,
-    "connect": 5,
     "backoff_factor": 1.3,
     "status_forcelist": (500, 502, 503, 504),
 }
